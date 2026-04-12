b2c8eb0075c04dba830a1e55d58eab289761eb64
edjnolasco <edjnolasco@gmail.com>
2026-04-12 18:23:31 -0400

    feat: 0604. Generador de contexto y repositorio country en proyecto Infrastructure.Tests

---
+++ INF.5120.Arch0604.Infrastructure.Tests/Context/ContextGenerator.cs
@@ -0,0 +1,15 @@
+﻿using INF._5120.Arch0604.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace INF._5120.Arch0604.Infrastructure.Tests.Context
+{
+    public static class ContextGenerator
+    {
+        public static INF5120DbContext Generate()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<INF5120DbContext>()
+                .UseInMemoryDatabase(databaseName: "INF5120");
+            return new INF5120DbContext(optionsBuilder.Options);
+        }
+    }
+}
